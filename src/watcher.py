8c8dcfb79dad39ce35f2c8dca561b0ed49752c60
Leo Lvcdb <leo.levancanhditban.etu@univ-lille.fr>
2021-08-30 22:44:31 +0200

    opti

--- src/watcher.py
+++ src/watcher.py
@@ -1,6 +1,6 @@
 import threading
 from Function import *
-
+import re
 
 class Watcher(threading.Thread) :
 
@@ -12,27 +12,46 @@ class Watcher(threading.Thread) :
 		self.path = path
 		#  liste of all files watched
 		self.data = []
+		self.dirs = []
 		# dict with all backup linked with the floder
-		self.backUp = {"01":path+"/.backup/B01"}
+		self.backUp = {"1":path+"/.backup/B1"}
+		self.Actualbackup = 1
 		# List of all ignored floder
 		self.ignore = ['.git','__pycache__','.backup']
 		# cache for all files of self.data
-		self.cached_stamp = {}
+		self.cached_stamp_floder = {}
+		self.cached_stamp_file = {}
 		# Creating backup floder
 		os.system("mkdir {}.backup".format(path+separator))
 		# first backup + reading data
 		for path, dirs, files in os.walk(self.path):
 			if all(not ignore in path.split(separator) for ignore in self.ignore):
+				for floder in dirs:
+					if not floder in self.ignore:
+						self.dirs.append(path+separator+floder)
+						self.cached_stamp_floder[path+separator+floder] = {"stamp":os.stat(path+separator+floder).st_size}
 				for file in files:
 					end = file.split('.')
 					if len(end) > 1 :
 						end = end[-1]
 						self.data.append(path+separator+file)
-						self.cached_stamp[path+separator+file] = {"stamp":os.stat(path+separator+file).st_mtime, "type":end, "backUp":"01"}
+						self.cached_stamp_file[path+separator+file] = {"stamp":os.stat(path+separator+file).st_mtime, "type":end, "backUp":"1"}
+		self.save()
+		self.save()
+		self.save()
+		self.save()
+		self.save()
 
+	def save(self):
+		for dirs in  self.dirs:
+			dirs =  dirs[len(self.path):]
+			os.system("mkdir {}.backup{}{}".format(self.path+separator,separator+"B"+str(self.Actualbackup),separator+dirs))
+		
 
+		self.Actualbackup +=1
+		self.backUp[str(self.Actualbackup)] = self.path+"/.backup/B"+str(self.Actualbackup)
 
 if __name__ == '__main__':
 	test = Watcher('E:\\Desktop\\Programmation\\gitDuBled')
-	print(test.data)
-	print(test.cached_stamp)
+	print(test.dirs)
+	print(test.cached_stamp_floder)
