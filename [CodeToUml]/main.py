35537a3a2f52f01dad76f2fdcf30cecfb7832542
Rouxhero <leolvcdb@gmail.com>
2021-10-04 18:42:56 +0200

    Update

--- [CodeToUml]/main.py
+++ [CodeToUml]/main.py
@@ -8,6 +8,27 @@ except Exception as e:
 	statu = False
 	print("OffLine Launch")
 
-if statu :
-	updateV = urllib.request.urlopen('https://www.github.com/Rouxhero/GitDuBled/')s
+
+
+def update():
+	allFIle = open("data/file","r").read().split('\n')
+	for file in allFIle:
+		if file[0] != "#":
+			url = "https://raw.githubusercontent.com/Rouxhero/GitDuBled/master/%5BCodeToUml%5D/{}".format(file)
+			print('Downloading : ',url,end=":")
+			data = urllib.request.urlopen(url).read().decode()
+			open(file,'w').write(data)
+			print("Done")
+	print("\nUpdate Done")
+print("Check Update : ",end="")
+# if statu :
+# 	updateV = urllib.request.urlopen('https://raw.githubusercontent.com/Rouxhero/GitDuBled/master/%5BCodeToUml%5D/data/version').read().decode()
+# 	version = open('data/version','r').read()
+# 	if version!=updateV:
+# 		print("Needs Update")
+# 		update()
+# 	else:
+# 		print("OK")
+
+update()
 
